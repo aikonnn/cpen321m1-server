a4ed6a1117297361ffad2196e75630e38c3eb5d7
Ubuntu <aikonnn@M1.m0hvcajmzdturh21m3nqk13kia.ux.internal.cloudapp.net>
2023-09-16 14:08:57 +0000

    working https server

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var os = require('os');
 var app = express();
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/aikonnn.canadacentral.cloudapp.azure.com/privkey.pem','utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/aikonnn.canadacentral.cloudapp.azure.com/cert.pem','utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/aikonnn.canadacentral.cloudapp.azure.com/chain.pem','utf8');
+
+const credentials = {
+	key:privateKey,
+	cert:certificate,
+	ca:ca
+};
 
 app.get('/server_ip', (req, res)=>{
     var interfaces = os.networkInterfaces();
@@ -26,6 +39,13 @@ app.get('/time', (req,res)=>{
     res.send(dateObject.toTimeString().slice(0,8))
 })
 
-var server = app.listen(8080, (req,res)=>{
-    console.log(server.address().port)
-})
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials,app);
+
+httpsServer.listen(443, (req,res)=>{
+    console.log('https 443')
+});
+
+httpServer.listen(80, () => {
+    console.log("http server online on 80");
+});
